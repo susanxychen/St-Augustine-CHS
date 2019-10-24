0e31897a50fcca0abb4c96fec5d1c6751808aa43
susanxychen <49922632+susanxychen@users.noreply.github.com>
2019-10-23 22:25:18 -0400

    Add files via upload

--- St Augustine CHS/FAQController.swift
+++ St Augustine CHS/FAQController.swift
@@ -37,22 +37,35 @@ class FAQController: UITableViewController{
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         if tableViewData[section].opened == true{
-            return tableViewData[section].sectionData.count
+            return tableViewData[section].sectionData.count + 1
         }else{
             return 1
         }
     }
     
-    override func tableView(_ tableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         if indexPath.row == 0{
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {return UITableViewCell()}
             cell.textLabel?.text = tableViewData[indexPath.section].title
             return cell
         }else{
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {return UITableViewCell()}
-            cell.textLabel?.text = tableViewData[indexPath.section].sectionData[indexPath.row]
+            cell.textLabel?.text = tableViewData[indexPath.section].sectionData[indexPath.row - 1]
             return cell
         }
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            if tableViewData[indexPath.section].opened == true {
+                tableViewData[indexPath.section].opened = false
+                let sections = IndexSet.init(integer: indexPath.section)
+                tableView.reloadSections(sections, with: .none)
+            } else {
+                tableViewData[indexPath.section].opened = true
+                let sections = IndexSet.init(integer: indexPath.section)
+                tableView.reloadSections(sections, with: .none)
+            }
+        }
+    }
 }
